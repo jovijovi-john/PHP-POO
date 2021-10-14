fb241d3af3bb4ef710991b977e119ae313fcfb8b
jovijovi-john <jovijovi2019@hotmail.com>
2021-10-14 15:25:18 -0300

    namespaces_pt1

---
+++ Aula-14/namespaces_pt1.md
@@ -0,0 +1,7 @@
+namespaces possibilitam agrupamento de classes, interfaces, funcoes e constantes. Visando evitar conflitos em seus respectivos nomes
+
+eles servem pra evitar conflitos de nomes de variaveis ou classes de bibliotecas com as da nossa aplicação
+
+supondo que criemos a classe Cliente, e importemos uma biblioteca, cuja qual tem a classe Cliente implementada dentro dela, nesse caso haverá um conflito pois são classes com nomes idênticos.
+
+\nomeDoNamespace\nomeDaClasse   Considerar o termo PATH como sendo namespace ex : \A\Classe ();
---
+++ Aula-14/namespaces_pt1.php
@@ -0,0 +1,84 @@
+<?php
+
+    namespace A;
+    
+    class Cliente implements CadastroInterface, \B\CadastroInterface{
+        // ta implementando a interface do namespace A e a do namespace B
+
+        public $nome = "John";
+
+        public function __construct() {
+            echo "<pre>";
+            print_r(get_class_methods($this));
+            echo "</pre>";
+        }
+        
+        public function __get($attr){
+            return $this->$attr;
+        }
+
+        
+        public function salvar(){
+            echo "Salvando";
+        }
+
+        public function remover(){
+            echo "removendo";
+        }
+    }
+
+    interface CadastroInterface {
+        public function salvar();
+    }
+
+    
+    
+    namespace B;
+    
+
+
+    class Cliente implements CadastroInterface, \A\CadastroInterface{
+        
+        public $nome = "Jovi";
+
+        
+        public function __construct() {
+            echo "<pre>";
+            print_r(get_class_methods($this));
+            echo "</pre>";
+        }
+
+
+        public function __get($attr){
+            return $this->$attr;
+        }
+
+        public function remover() {
+            echo "removendo";
+        }
+        
+        public function salvar(){
+            echo "Salvando";
+        }
+    }
+
+    
+    interface CadastroInterface {
+        public function remover();
+    }
+
+    // $c = new Cliente();
+    // echo "<pre>";
+    // print_r($c);
+    // echo "</pre>";
+    // echo $c->__get("nome");
+    // // aqui vai printar o "Jovi", isso porque ainda estamos no namespace b
+
+    
+    $c = new \B\Cliente();
+    echo "<pre>";
+    print_r($c);
+    echo "</pre>";
+    echo $c->__get("nome");
+
+?>
